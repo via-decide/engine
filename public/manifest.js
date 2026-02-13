235b44758224d2042eacf8cfd0fc087302f240dc
via-decide <via.decide@gmail.com>
2026-02-13 16:13:37 +0530

    Update manifest.js

--- public/manifest.js
+++ public/manifest.js
@@ -1,15 +1,9 @@
 {
   "name": "Decide Engine",
-  "short_name": "DecideOS",
-  "start_url": "/#/",
+  "short_name": "Decide",
+  "start_url": "/",
   "display": "standalone",
-  "background_color": "#000000",
+  "background_color": "#050505",
   "theme_color": "#000000",
-  "icons": [
-    {
-      "src": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAF0lEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAK4G4mAAAXc6lS8AAAAASUVORK5CYII=",
-      "sizes": "192x192",
-      "type": "image/png"
-    }
-  ]
+  "icons": []
 }
