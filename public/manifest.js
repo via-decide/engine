2d9590ceee7464c769bf2d458b65d5d7f6356131
via-decide <via.decide@gmail.com>
2026-02-13 13:53:14 +0530

    Create manifest.js

---
+++ public/manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Decide Engine",
+  "short_name": "DecideOS",
+  "start_url": "/#/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAF0lEQVR4nO3BMQEAAADCoPVPbQ0PoAAAAAAAAK4G4mAAAXc6lS8AAAAASUVORK5CYII=",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
